cdfc25b2b1b63c793663be9aeac1c3606069e3b7
Robert Mustacchi <rm@joyent.com>
2011-06-06 14:02:23 -0700

    HVM-306 kvm_cache_regs.h missing forward declarations

--- kvm_cache_regs.h
+++ kvm_cache_regs.h
@@ -1,8 +1,11 @@
 #ifndef ASM_KVM_CACHE_REGS_H
 #define ASM_KVM_CACHE_REGS_H
 
-#include "processor-flags.h"
 #include <sys/types.h>
+
+#include "processor-flags.h"
+
+enum kvm_reg;
 
 #define KVM_POSSIBLE_CR0_GUEST_BITS X86_CR0_TS
 #define KVM_POSSIBLE_CR4_GUEST_BITS				  \
