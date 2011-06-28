ff09c7a12308ea886df0f5e0fcf7a2210f964415
Robert Mustacchi <rm@joyent.com>
2011-06-28 15:28:15 -0700

    HVM-439 kvm_iodev.h should be cstyle clean

--- kvm_iodev.h
+++ kvm_iodev.h
@@ -14,27 +14,21 @@
  */
 
 #ifndef __KVM_IODEV_H__
-#define __KVM_IODEV_H__
+#define	__KVM_IODEV_H__
 
 #include "kvm_types.h"
 
 struct kvm_io_device;
 
-/**
+/*
  * kvm_io_device_ops are called under kvm slots_lock.
  * read and write handlers return 0 if the transaction has been handled,
  * or non-zero to have it passed to the next device.
- **/
+ */
 typedef struct kvm_io_device_ops {
-	int (*read)(struct kvm_io_device *this,
-		    gpa_t addr,
-		    int len,
-		    void *val);
-	int (*write)(struct kvm_io_device *this,
-		     gpa_t addr,
-		     int len,
-		     const void *val);
-	void (*destructor)(struct kvm_io_device *this);
+	int (*read)(struct kvm_io_device *, gpa_t, int, void *);
+	int (*write)(struct kvm_io_device *, gpa_t, int, const void *);
+	void (*destructor)(struct kvm_io_device *);
 } kvm_io_device_ops_t;
 
 
@@ -43,15 +37,15 @@ typedef struct kvm_io_device {
 	const struct kvm_io_device_ops *ops;
 } kvm_io_device_t;
 
-void kvm_iodevice_init(struct kvm_io_device *,
+extern void kvm_iodevice_init(struct kvm_io_device *,
     const struct kvm_io_device_ops *);
 
-int kvm_iodevice_read(struct kvm_io_device *,
+extern int kvm_iodevice_read(struct kvm_io_device *,
     gpa_t, int, void *);
 
-int kvm_iodevice_write(struct kvm_io_device *dev,
-     gpa_t addr, int l, const void *v);
+extern int kvm_iodevice_write(struct kvm_io_device *,
+    gpa_t, int, const void *);
 
-void kvm_iodevice_destructor(struct kvm_io_device *dev);
+extern void kvm_iodevice_destructor(struct kvm_io_device *);
 
 #endif /* __KVM_IODEV_H__ */
