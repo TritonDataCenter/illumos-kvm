fbab7df6cea9fa3396ffea709186398d4021d87b
Robert Mustacchi <rm@joyent.com>
2011-06-28 11:04:42 -0700

    HVM-433 kvm_i8254.h should be c-style clean HVM-434 Make kvm binary depend on kvm header files

--- kvm_impl.h
+++ kvm_impl.h
@@ -8,9 +8,9 @@
 #include <sys/kstat.h>
 #include <sys/sdt.h>
 
-#define XXX_KVM_PROBE DTRACE_PROBE2(kvm__xxx, \
+#define	XXX_KVM_PROBE DTRACE_PROBE2(kvm__xxx, \
 	char *, __FILE__, int, __LINE__)
-#define XXX_KVM_SYNC_PROBE DTRACE_PROBE2(kvm__xxx__sync, \
+#define	XXX_KVM_SYNC_PROBE DTRACE_PROBE2(kvm__xxx__sync, \
 	char *, __FILE__, int, __LINE__)
 
 #define	KVM_TRACE1(name, type1, arg1)					\
@@ -38,60 +38,60 @@
 	    type3, arg3, type4, arg4, type5, arg5, type6, arg6);
 
 typedef struct kvm_vcpu_stats {
-       kstat_named_t kvmvs_id;                 /* instance of associated kvm */
-       kstat_named_t kvmvs_nmi_injections;     /* number of NMI injections */
-       kstat_named_t kvmvs_irq_injections;     /* number of IRQ injections */
-       kstat_named_t kvmvs_fpu_reload;         /* number of FPU reloads */
-       kstat_named_t kvmvs_host_state_reload;  /* host state (re)loads */
-       kstat_named_t kvmvs_insn_emulation;     /* instruction emulation */
-       kstat_named_t kvmvs_insn_emulation_fail; /* emulation failures */
-       kstat_named_t kvmvs_exits;              /* total VM exits */
-       kstat_named_t kvmvs_halt_exits;         /* exits due to HLT */
-       kstat_named_t kvmvs_irq_exits;          /* exits due to IRQ */
-       kstat_named_t kvmvs_io_exits;           /* exits due to I/O instrn */
-       kstat_named_t kvmvs_mmio_exits;         /* exits due to mem mppd I/O */
-       kstat_named_t kvmvs_nmi_window_exits;   /* exits due to NMI window */
-       kstat_named_t kvmvs_irq_window_exits;   /* exits due to IRQ window */
-       kstat_named_t kvmvs_request_irq_exits;  /* exits due to requested IRQ */
-       kstat_named_t kvmvs_signal_exits;       /* exits due to pending sig */
-       kstat_named_t kvmvs_halt_wakeup;        /* wakeups from HLT */
-       kstat_named_t kvmvs_invlpg;             /* INVLPG instructions */
-       kstat_named_t kvmvs_pf_guest;           /* injected guest pagefaults */
-       kstat_named_t kvmvs_pf_fixed;           /* fixed pagefaults */
-       kstat_named_t kvmvs_hypercalls;         /* hypercalls (VMCALL instrn) */
+	kstat_named_t kvmvs_id;			/* instance of associated kvm */
+	kstat_named_t kvmvs_nmi_injections;	/* number of NMI injections */
+	kstat_named_t kvmvs_irq_injections;	/* number of IRQ injections */
+	kstat_named_t kvmvs_fpu_reload;		/* number of FPU reloads */
+	kstat_named_t kvmvs_host_state_reload;	/* host state (re)loads */
+	kstat_named_t kvmvs_insn_emulation;	/* instruction emulation */
+	kstat_named_t kvmvs_insn_emulation_fail;	/* emulation failures */
+	kstat_named_t kvmvs_exits;		/* total VM exits */
+	kstat_named_t kvmvs_halt_exits;		/* exits due to HLT */
+	kstat_named_t kvmvs_irq_exits;		/* exits due to IRQ */
+	kstat_named_t kvmvs_io_exits;		/* exits due to I/O instrn */
+	kstat_named_t kvmvs_mmio_exits;		/* exits due to mem mppd I/O */
+	kstat_named_t kvmvs_nmi_window_exits;	/* exits due to NMI window */
+	kstat_named_t kvmvs_irq_window_exits;	/* exits due to IRQ window */
+	kstat_named_t kvmvs_request_irq_exits;	/* exits due to requested IRQ */
+	kstat_named_t kvmvs_signal_exits;	/* exits due to pending sig */
+	kstat_named_t kvmvs_halt_wakeup;	/* wakeups from HLT */
+	kstat_named_t kvmvs_invlpg;		/* INVLPG instructions */
+	kstat_named_t kvmvs_pf_guest;		/* injected guest pagefaults */
+	kstat_named_t kvmvs_pf_fixed;		/* fixed pagefaults */
+	kstat_named_t kvmvs_hypercalls;		/* hypercalls (VMCALL instrn) */
 } kvm_vcpu_stats_t;
 
-#define KVM_VCPU_KSTAT_INIT(vcpu, field, name) \
+#define	KVM_VCPU_KSTAT_INIT(vcpu, field, name) \
 	kstat_named_init(&((vcpu)->kvcpu_stats.field), name, KSTAT_DATA_UINT64);
 
-#define KVM_VCPU_KSTAT_INC(vcpu, field) \
+#define	KVM_VCPU_KSTAT_INC(vcpu, field) \
 	(vcpu)->kvcpu_stats.field.value.ui64++;
 
 typedef struct kvm_stats {
-       kstat_named_t kvmks_pid;                /* PID of opening process */
-       kstat_named_t kvmks_mmu_pte_write;      /* page table entry writes */
-       kstat_named_t kvmks_mmu_pte_zapped;     /* zapped page table entries */
-       kstat_named_t kvmks_mmu_pte_updated;    /* page table entry updates */
-       kstat_named_t kvmks_mmu_flooded;        /* # of pages flooded */
-       kstat_named_t kvmks_mmu_cache_miss;     /* misses in page cache */
-       kstat_named_t kvmks_mmu_recycled;       /* recycles from free list */
-       kstat_named_t kvmks_remote_tlb_flush;   /* remote TLB flushes */
-       kstat_named_t kvmks_lpages;             /* large pages in use */
-	kstat_named_t kvmks_mmu_unsync_page;   /* number of unsynced pages */
+	kstat_named_t kvmks_pid;		/* PID of opening process */
+	kstat_named_t kvmks_mmu_pte_write;	/* page table entry writes */
+	kstat_named_t kvmks_mmu_pte_zapped;	/* zapped page table entries */
+	kstat_named_t kvmks_mmu_pte_updated;	/* page table entry updates */
+	kstat_named_t kvmks_mmu_flooded;	/* # of pages flooded */
+	kstat_named_t kvmks_mmu_cache_miss;	/* misses in page cache */
+	kstat_named_t kvmks_mmu_recycled;	/* recycles from free list */
+	kstat_named_t kvmks_remote_tlb_flush;	/* remote TLB flushes */
+	kstat_named_t kvmks_lpages;		/* large pages in use */
+	kstat_named_t kvmks_mmu_unsync_page;	/* number of unsynced pages */
 } kvm_stats_t;
 
-#define KVM_KSTAT_INIT(kvmp, field, name) \
+#define	KVM_KSTAT_INIT(kvmp, field, name) \
 	kstat_named_init(&((kvmp)->kvm_stats.field), name, KSTAT_DATA_UINT64);
 
-#define KVM_KSTAT_INC(kvmp, field) \
+#define	KVM_KSTAT_INC(kvmp, field) \
 	(kvmp)->kvm_stats.field.value.ui64++;
 
-#define KVM_KSTAT_DEC(kvmp, field) \
+#define	KVM_KSTAT_DEC(kvmp, field) \
 	(kvmp)->kvm_stats.field.value.ui64--;
 
 
 #ifndef ARRAY_SIZE
-#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
-#endif /*ARRAY_SIZE*/
+#define	ARRAY_SIZE(x) (sizeof (x)/sizeof (x[0]))
+#endif /* ARRAY_SIZE */
 
 #endif
